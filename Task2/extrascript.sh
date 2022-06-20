388ae77c3331e8731122cfb251258992d46a0984
Alexey Krasichonak <83491125+alexeykrasich@users.noreply.github.com>
2022-06-20 14:04:00 +0300

    Added checking distr

--- Task2/extrascript.sh
+++ Task2/extrascript.sh
@@ -1,5 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#Apache downloading
-apt install apache2 -y
-echo "<h1>Hello World<br>$(uname -a)</h1>" > /var/www/html/index.html
+RELEASE=$(cat /etc/*release)
+DISTR_AMAZON=$(echo $RELEASE | grep mazon)
+DISTR_UBUNTU=$(echo $RELEASE | grep buntu)
+DISTR_CENTOS=$(echo $RELEASE | grep entOS)
+
+#######Distr checking
+
+
+if [ "$DISTR_AMAZON" != "" ]; then
+        echo "Your distr is Amazon Linux"
+        yum -y install httpd && service httpd start ; echo "Hello World<br>$(uname -a)" > /var/www/html/index.html
+elif [ "$DISTR_UBUNTU" != "" ]; then
+        echo "Your distr is Ubuntu"
+        apt install apache2 -y && echo "Hello World<br>$(uname -a)" > /var/www/html/index.html
+elif [ "$DISTR_CENTOS" != "" ]; then
+        echo "Your distr is CentOS"
+        yum -y install httpd && service httpd start ; echo "Hello World<br>$(uname -a)" > /var/www/html/index.html
+else
+        echo "Don't know your distr"
+fi
