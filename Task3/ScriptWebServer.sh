1bb9b4842679e676bebdbb0fb63b9d4043ddc49f
Alexey Krasichonak <alexey.krasich@gmail.com>
2022-06-24 05:35:03 -0700

    Final version 1-5

--- Task3/ScriptWebServer.sh
+++ Task3/ScriptWebServer.sh
@@ -39,9 +39,3 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-#apt -y install apt-transport-https
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#apt update
-#apt -y install docker-ce
