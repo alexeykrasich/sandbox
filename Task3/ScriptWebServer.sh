7764d16d8fe88d97ab8d319cff24515ce038d17a
Alexey Krasichonak <alexey.krasich@gmail.com>
2022-06-24 03:13:32 -0700

    Added full docker instalation

--- Task3/ScriptWebServer.sh
+++ Task3/ScriptWebServer.sh
@@ -26,4 +26,22 @@ fi
 
 ####################INSTALL DOCKER######
 
-apt update && apt install apt-transport-https
+apt update
+apt -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#apt -y install apt-transport-https
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#apt update
+#apt -y install docker-ce
